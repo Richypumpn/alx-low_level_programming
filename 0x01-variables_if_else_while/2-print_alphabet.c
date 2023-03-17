ca54e6061dfabc2216f2152a56c74d7df704c4d5
Richard Ogbonnah <richypumpn@gmail.com>
2023-03-17 14:19:32 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,10 +9,10 @@ int main(void)
 {
 	int i = 97;
 	while (i <= 122)
-}
-putchar((char) i);
-i++;
-}
-putchar((char)'\n');
-return (0);
+	{
+		putchar((char) i);
+		i++;
+	}
+	putchar((char)'\n');
+	return (0);
 }
