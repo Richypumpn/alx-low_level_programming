e3f7b11c39d28ad46a3c72894d2fec40a7afbb9a
Richard Ogbonnah <richypumpn@gmail.com>
2023-03-17 15:38:38 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: print a-z
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 48;
+
+	while (i <= 57)
+	{
+		putchar((char) i);
+		i++;
+	}
+	putchar((char)'\n');
+	return (0);
+}
