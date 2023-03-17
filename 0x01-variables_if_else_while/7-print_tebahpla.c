58426565e08c28d27130111c25695f037a666fd3
Richard Ogbonnah <richypumpn@gmail.com>
2023-03-17 15:45:34 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: print a-z
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 122;
+
+	while (i >= 97)
+	{
+		putchar((char) i);
+		i--;
+	}
+	putchar((char)'\n');
+	return (0);
+}
