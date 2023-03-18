3bf0e5265cc221d6a7a8f3aa3116ee31a0e730f0
Richard Ogbonnah <richypumpn@gmail.com>
2023-03-18 04:10:25 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,8 +2,8 @@
 
 /**
  * main - Entry point
- * Description: print two digits combination
- * Return: Always 0 (success)
+ * Description: print
+ * Return: 0 (success)
  */
 int main(void)
 {
@@ -14,7 +14,8 @@ for (c = '0'; c <= '9'; c++)
 for (i = '0'; i <= '9'; i++)
 {
 if (c < i)
-{putchar(c);
+{
+putchar(c);
 putchar(i);
 if (c != '8' || (c == '8' && i != '9'))
 {
