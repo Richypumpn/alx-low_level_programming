26b9fc93c3476ae800258b959204af8783492345
Richard Ogbonnah <richypumpn@gmail.com>
2023-03-18 00:48:28 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,24 +7,21 @@
  */
 int main(void)
 {
-	int n, m, l;
+	int c, i;
 
-	for (n = 48; n < 58; n++)
+	for (c = '0'; c <= '9'; c++)
 	{
-		for (m = 49; m < 58; m++)
+		for (i = '0'; i <= '9'; i++)
 		{
-			for (l = 50; l < 58; l++)
+			if (c < i)
 			{
-				if (l > m && m > n)
+				putchar(c);
+				putchar(i);
+
+				if (c != '8' || (c == '8' && i != '9'))
 				{
-					putchar(n);
-					putchar(m);
-					putchar(l);
-					if (n != 55 || m != 56)
-					{
-						putchar(',');
-						putchar(' ');
-					}
+					putchar(',');
+					putchar(' ');
 				}
 			}
 		}
